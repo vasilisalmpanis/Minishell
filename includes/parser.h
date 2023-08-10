bc6ec0586aa47be07d59dc2cec9af18364b1f211
mamesser <mamesser@student.42wolfsburg.de>
2023-08-10 13:23:06 +0200

    updated header files for parsing

--- includes/parser.h
+++ includes/parser.h
@@ -6,7 +6,7 @@
 /*   By: mamesser <mamesser@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/07 17:20:07 by valmpani          #+#    #+#             */
-/*   Updated: 2023/08/09 19:08:04 by mamesser         ###   ########.fr       */
+/*   Updated: 2023/08/10 12:37:10 by mamesser         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,13 +37,20 @@ typedef struct s_cmd
 	struct s_cmd	*next;
 }				t_cmd;
 
-t_cmd	*parse(t_lex *lex_lst);
-int		analyze_token(t_lex **lex_lst, t_cmd *new_cmd, int *arg_num);
+t_cmd	*parse(t_lex *lex_lst, char **env_paths);
+int		analyze_token(t_lex **lex_lst, t_cmd *new_cmd, int *arg_num, char **env_paths);
 int		set_here_doc_flag(t_lex **lex_lst, t_cmd *new_cmd);
 int		set_redir_flags(t_lex **lex_lst, t_cmd *new_cmd);
 int		allocate_args(t_lex *lex_lst, t_cmd *new_cmd);
 
-int		analyze_word(t_lex **lex_lst, t_cmd *new_cmd, int *arg_num);
+int		analyze_word(t_lex **lex_lst, t_cmd *new_cmd, int *arg_num, char **env_paths);
+int		analyze_cmd(t_lex **lex_lst, t_cmd *new_cmd, int *arg_num, char **env_paths);
 int		check_word_type(t_lex *lex_lst, int hd_flag);
+char	*get_path_name(t_cmd *new_cmd, char **env_paths);
+int		get_cmd_name(t_lex **lex_lst, t_cmd *new_cmd);
+int		check_for_builtin(t_lex **lex_lst, t_cmd *new_cmd);
+int		check_word_type(t_lex *lex_lst, int in_flag);
+void	check_for_path(t_lex **lex_lst, t_cmd *new_cmd);
+int		analyze_opt(t_lex **lex_lst, t_cmd *new_cmd);
 
 #endif
