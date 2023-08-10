bbead82e9fe8296f6701925b2482d0318597619c
mamesser <mamesser@student.42wolfsburg.de>
2023-08-10 17:11:56 +0200

    updated parser header file

--- includes/parser.h
+++ includes/parser.h
@@ -6,7 +6,7 @@
 /*   By: mamesser <mamesser@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/07 17:20:07 by valmpani          #+#    #+#             */
-/*   Updated: 2023/08/10 12:37:10 by mamesser         ###   ########.fr       */
+/*   Updated: 2023/08/10 17:01:27 by mamesser         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,13 @@
 # define PARSER_H
 
 # include "minishell.h"
+
+// typedef struct s_parse_vars
+// {
+// 	int	id;
+// 	int	arg_num;
+// }				t_vars;
+
 
 typedef struct s_cmd
 {
@@ -37,20 +44,23 @@ typedef struct s_cmd
 	struct s_cmd	*next;
 }				t_cmd;
 
-t_cmd	*parse(t_lex *lex_lst, char **env_paths);
-int		analyze_token(t_lex **lex_lst, t_cmd *new_cmd, int *arg_num, char **env_paths);
+t_cmd	*parser(t_lex *lex_lst, char **env_paths);
+int		parse_tokens(t_lex *lex_lst, t_cmd **cmd_lst, char **env_paths);
+int		allocate_args(t_lex *lex_lst, t_cmd *new_cmd);
+int		analyze_token(t_lex **lex_lst, t_cmd *new_cmd, int *arg_num, char **env_p);
+
 int		set_here_doc_flag(t_lex **lex_lst, t_cmd *new_cmd);
 int		set_redir_flags(t_lex **lex_lst, t_cmd *new_cmd);
-int		allocate_args(t_lex *lex_lst, t_cmd *new_cmd);
 
-int		analyze_word(t_lex **lex_lst, t_cmd *new_cmd, int *arg_num, char **env_paths);
-int		analyze_cmd(t_lex **lex_lst, t_cmd *new_cmd, int *arg_num, char **env_paths);
-int		check_word_type(t_lex *lex_lst, int hd_flag);
-char	*get_path_name(t_cmd *new_cmd, char **env_paths);
-int		get_cmd_name(t_lex **lex_lst, t_cmd *new_cmd);
-int		check_for_builtin(t_lex **lex_lst, t_cmd *new_cmd);
+int		analyze_word(t_lex **lex_lst, t_cmd *new_cmd, int *arg_num, char **env_p);
 int		check_word_type(t_lex *lex_lst, int in_flag);
-void	check_for_path(t_lex **lex_lst, t_cmd *new_cmd);
+int		analyze_cmd(t_lex **lex_lst, t_cmd *new_cmd, int *arg_num, char **env_p);
 int		analyze_opt(t_lex **lex_lst, t_cmd *new_cmd);
+
+int		check_for_builtin(t_lex **lex_lst, t_cmd *new_cmd);
+void	check_for_path(t_lex **lex_lst, t_cmd *new_cmd);
+int		get_cmd_name(t_lex **lex_lst, t_cmd *new_cmd);
+char	*get_path_name(t_cmd *new_cmd, char **env_paths);
+
 
 #endif
