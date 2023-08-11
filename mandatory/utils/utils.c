c9408241835012fe257747d5072b3662e3e6470b
mamesser <mamesser@student.42wolfsburg.de>
2023-08-11 12:09:56 +0200

    handling redirects without space

--- mandatory/utils/utils.c
+++ mandatory/utils/utils.c
@@ -6,7 +6,7 @@
 /*   By: mamesser <mamesser@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/06 15:52:47 by valmpani          #+#    #+#             */
-/*   Updated: 2023/08/07 15:47:51 by mamesser         ###   ########.fr       */
+/*   Updated: 2023/08/11 12:08:38 by mamesser         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,8 +38,61 @@ char	**split_args(char *input)
 				j++;
 		}
 	}
+	input = handle_redirects(input, 0);
+	if (!input)
+		return (NULL);
 	split = ft_split(input, 26);
 	return (split);
+}
+
+char	*handle_redirects(char *input, int j)
+{
+	int		i;
+	char	*new_input;
+	int		red_no_sp;
+
+	i = -1;
+	red_no_sp = calc_redir_wo_space(input);
+	new_input = malloc((ft_strlen(input) + red_no_sp + 1) * sizeof(*new_input));
+	if (!new_input)
+		return (NULL);
+	while (input[++i])
+	{
+		new_input[j++] = input[i];
+		if ((input[i] == '<' || input[i] == '>')
+			&& (input[i + 1] != '<' && input[i + 1] != '>'
+			&& !(ft_isspace(input[i + 1]))))
+		{
+			new_input[j++] = ' ';
+			if (i > 0 && (input[i] == '<' && input[i - 1] == '>')
+				|| (input[i] == '>' && input[i - 1] == '<'))
+				j--;
+		}
+	}
+	new_input[j] = '\0';
+	return (new_input);
+}
+
+int	calc_redir_wo_space(char *input)
+{
+	int	redir_no_sp;
+	int	i;
+
+	redir_no_sp = 0;
+	i = 0;
+	while (input[++i + 1])
+	{
+		if ((input[i] == '<' || input[i] == '>')
+			&& (input[i + 1] != '<' && input[i + 1] != '>'
+			&& !(ft_isspace(input[i + 1]))))
+		{
+			redir_no_sp++;
+			if (i > 0 && (input[i] == '<' && input[i - 1] == '>')
+				|| (input[i] == '>' && input[i - 1] == '<'))
+				redir_no_sp--;
+		}
+	}
+	return (redir_no_sp);
 }
 
 char	*ft_charjoin_mod(char *s1, char s2)
