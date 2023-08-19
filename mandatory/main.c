c3c01fe16e1b3c07b7ccdf64fe76458a24ff4e49
valmpani <vasilisalmpanis@gmail.com>
2023-08-19 09:09:09 +0200

    basic tests running

--- mandatory/main.c
+++ mandatory/main.c
@@ -12,13 +12,12 @@
 
 #include "../includes/minishell.h"
 
-void	exit_min(char *input, t_lex **lst)
+void	exit_min(char *input, t_lex **lst, int exit_code)
 {
-	printf("Exiting minishell\n");
 	(void)lst;
 	if (input)
 		free(input);
-	exit(1);
+	exit(exit_code);
 }
 
 int	main(int argc, char **argv, char **envp)
@@ -45,7 +44,7 @@ int	main(int argc, char **argv, char **envp)
 		input[1] = readline(input[0]); //input[1] is allocated?
 		free(input[0]);
 		if (!input[1])
-			exit_min(input[1], &lex_lst);
+			exit_min(input[1], &lex_lst, exit_code);
 		input[0] = ft_strdup(input[1]); //input[0] is allocated / gets freed in lexer split_args
 		if (!input[0])
 			return (1);
@@ -61,7 +60,7 @@ int	main(int argc, char **argv, char **envp)
 				exit_code = execute(cmd_lst, env_lst, exit_code);
 		}
 		if (strcmp(input[1], "exit") == 0)
-			exit_min(input[1], &lex_lst);
+			exit_min(input[1], &lex_lst, exit_code);
 		add_history(input[1]);
 		free(input[1]);
 		ft_cmd_lst_free(&cmd_lst);
@@ -103,74 +102,3 @@ void	ft_show_tab2(t_cmd *list)
 		list = list->next;
 	}
 }
-
-
-
-/* ----------------------TEST MAIM---------------------- */
-//void	parse(char *input, t_env **lst)
-//{
-//	t_cmd cmd1;
-//	t_env *node;
-//	char **temp;
-//	char **var;
-//
-//	cmd1.args = NULL;
-//	if (strcmp(input, "env") == 0)
-//		env(*lst, cmd1);
-//	else if (ft_strncmp(input, "export", 6) == 0)
-//	{
-//		int i = 0;
-//		temp = split_args(input);
-//		cmd1.args = ft_calloc(2, 1);
-//		while (temp[i])
-//			++i;
-//		if (i == 1)
-//		{
-//			cmd1.args = NULL;
-//			export(lst, &cmd1);
-//			return;
-//		}
-//		cmd1.args[0] = temp[1];
-//		var = ft_split(cmd1.args[0], '=');
-//		export(lst, &cmd1);
-//	}
-//	else if (ft_strncmp(input, "unset", 5) == 0)
-//	{
-//		temp = ft_split(input,  ' ');
-//		ft_env_remove(lst, temp[1]);
-//		return ;
-//	}
-//	if (strcmp(input, "..") == 0)
-//		if (chdir("..") != 0)
-//			perror("cd");
-//	if (strcmp(input, "cd") == 0)
-//		chdir("test");
-//	if (!input)
-//		exit_min(input, NULL);
-//	if (strcmp(input, "exit") == 0)
-//	{
-//		exit_min(input, NULL);
-//	}
-//}
-//
-//int	main(int argc, char **argv, char **en)
-//{
-//	char				*input;
-//	char				*temp;
-//	t_cmd 				cmd1;
-//	t_env				*lst;
-//
-//	argc++;
-//	argv++;
-//	cmd1.args = NULL;
-//	lst = create_env(en);
-//	while (1)
-//	{
-//		temp = prompt();
-//		input = readline(temp);
-//		free(temp);
-//		parse(input, &lst);
-//		add_history(input);
-//		free(input);
-//	}
-//}
